ddfb1ed5b5e489adf6a31d1ba59b0655378f4df7
Christoph Bühler <christoph@smartive.ch>
2020-05-08 13:36:40 +0200

    chore: make buildfile executable

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ $TempDirectory = "$PSScriptRoot\\.tmp"
 
 $DotNetGlobalFile = "$PSScriptRoot\\global.json"
 $DotNetInstallUrl = "https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1"
-$DotNetChannel = "Current"
+$DotNetChannel = "LTS"
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
